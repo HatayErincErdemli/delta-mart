8344b06dd0e98bf12ec7b3a74035fa679c9f4072
HatayErincErdemli <hatayerinc@gmail.com>
2024-10-25 19:02:58 +0300

    Shop Page Component tamamlandı

--- Delta Mart/src/pages/ShopPage.jsx
+++ Delta Mart/src/pages/ShopPage.jsx
@@ -1,10 +1,14 @@
+import Footer from "@/layout/Footer";
 import Header from "@/layout/Header";
+import ShopPageContent from "@/layout/ShopPageContent";
 import React from "react";
 
 function ShopPage() {
   return (
     <div>
       <Header />
+      <ShopPageContent />
+      <Footer />
     </div>
   );
 }
